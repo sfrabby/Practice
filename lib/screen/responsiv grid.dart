0f4c7a0c68fe27cbe8a14cfa60083a97a099eabe
sfrabby <fazlerabby6732@gmail.com>
2026-05-13 23:39:14 +0600

    responsiv grid

---
+++ lib/screen/responsiv grid.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
+
+class responsivGrid extends StatelessWidget {
+  const responsivGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Responsiv Grid", style: TextStyle(color: Colors.white),),
+      ),
+      body: ResponsiveGridRow(children: [
+        ResponsiveGridCol(
+
+            child: Container(color: Colors.green,))
+      ]),
+    );
+  }
+}
