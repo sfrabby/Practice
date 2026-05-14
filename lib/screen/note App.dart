777b618c1afa42ee1c86d5db0d7333bbd4946eff
sfrabby <fazlerabby6732@gmail.com>
2026-05-15 00:46:20 +0600

    task controller added

--- lib/screen/note App.dart
+++ lib/screen/note App.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
 
-class noteApp extends StatelessWidget {
-  const noteApp({super.key});
+class noteApp extends StatefulWidget {
+   noteApp({super.key});
+
+  @override
+  State<noteApp> createState() => _noteAppState();
+}
+
+class _noteAppState extends State<noteApp> {
+  TextEditingController taskController = TextEditingController();
+
+  List<String> taskList = [];
+
+  void taskAdd(){
+    if(taskController.text.isNotEmpty){
+      setState(() {
+        taskList.add(taskController.text);
+        taskController.clear();
+      });
+    }
+  }
+  void deleteTask(int index){
+    setState(() {
+      taskList.removeAt(index);
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("To-DO app", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),),
+        backgroundColor: Colors.teal,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(child: TextField(
+                  controller: taskController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+
+                    )
+                  ),
+                )),
+                Expanded(child: ElevatedButton(onPressed: (){}, child: Text("Add ")))
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
