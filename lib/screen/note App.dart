143b23c416053bc7adcded97ab67e01719cb5f10
sfrabby <fazlerabby6732@gmail.com>
2026-05-14 23:51:31 +0600

    todo app

---
+++ lib/screen/note App.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class noteApp extends StatelessWidget {
+  const noteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
