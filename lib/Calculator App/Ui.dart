3d562abb0096577c9c4f1d435bb388dfdee731ef
sfrabby <fazlerabby6732@gmail.com>
2026-05-17 23:27:11 +0600

    Refactor widget

--- lib/Calculator App/Ui.dart
+++ lib/Calculator App/Ui.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Widget/button.dart';
+
 class calculator extends StatefulWidget {
   const calculator({super.key});
 
@@ -13,30 +15,49 @@ class _calculatorState extends State<calculator> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.black ,
-        title:Text("calculator", style: TextStyle(color: Colors.white, fontSize: 25,  fontWeight: FontWeight.w700),) ,
+        backgroundColor: Colors.black,
+        title: Text(
+          "calculator",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              alignment: Alignment.topRight ,
+              alignment: Alignment.topRight,
               height: 250,
               width: double.infinity,
-              child: Text("1234", style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w800),),
-            ),
-            SizedBox(height: 10,),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.grey
+              child: Text(
+                "1234",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.w800,
                 ),
-                onPressed: (){}, child: Text("1", style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w700),))
-
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                BuildButton(onClick: () {  }, text: '1',),
+                SizedBox(width: 20,),
+                BuildButton(onClick: () {  }, text: '2',),
+                SizedBox(width: 20,),
+                BuildButton(onClick: () {  }, text: '3',)
+              ],
+            )
           ],
         ),
       ),
     );
   }
 }
+
---
+++ lib/Calculator App/Widget/button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class BuildButton extends StatelessWidget {
+  String text;
+  Color? color;
+  final VoidCallback onClick;
+   BuildButton({
+    super.key, required this.onClick,
+     this.color, required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.all(10),
+        fixedSize: Size(40, 60),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        backgroundColor: Colors.grey[300],
+      ),
+      onPressed: onClick,
+      child: Text(
+        text,
+        style: TextStyle(
+          color: color ?? Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+}
