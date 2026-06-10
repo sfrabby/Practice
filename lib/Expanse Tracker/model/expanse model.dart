93ab16562ffc23eb0c21c2c4e55eafd676a8a1eb
sfrabby <fazlerabby6732@gmail.com>
2026-06-10 23:48:58 +0600

    Model added

---
+++ lib/Expanse Tracker/model/expanse model.dart
@@ -0,0 +1,16 @@
+ class Expanse{
+  String title;
+  double amount ;
+  String category;
+  DateTime date;
+
+
+  Expanse({
+  required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+});
+
+}
+
